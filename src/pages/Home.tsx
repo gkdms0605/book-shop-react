f78ed4b1e4e170233afa9695566b3faacdef13d2
gkdms0605 <leehaeun060523@gmail.com>
2025-07-20 04:27:08 +0900

    신간 섹션 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,21 +2,29 @@ import styled from "styled-components";
 import MainReview from "@/components/main/MainReview";
 import { useMain } from "@/hooks/useMain";
 import Title from "@/components/common/Title";
+import MainNewBooks from "@/components/main/MainNewBooks";
 
 function Home() {
-  const {reviews} = useMain();
+  const {reviews, newBooks} = useMain();
 
   return (
     <HomeStyle>
       {}
       {}
-      {}
-      <Title size={"large"}>리뷰</Title>
-      {<MainReview reviews={reviews}/>}
+      <section>
+        <Title size={"large"}>신간</Title>
+        {<MainNewBooks books={newBooks} />}
+      </section>
+      <section>
+        <Title size={"large"}>리뷰</Title>
+        {<MainReview reviews={reviews}/>}
+      </section>
     </HomeStyle>
   );
 }
 
-const HomeStyle = styled.div``;
+const HomeStyle = styled.div`
+
+`;
 
 export default Home;
